9a9086b8b293754072ae2f6c9fa95bda95a7a740
yassinenaciri <yassinen84@gmail.com>
2022-03-10 16:50:17 +0100

    7

--- src/test/java/FizzBuzzConverterTest.java
+++ src/test/java/FizzBuzzConverterTest.java
@@ -4,39 +4,45 @@ import org.junit.Test;
 public class FizzBuzzConverterTest {
     @Test
     public void FizzBuzzshouldReturn1whenGiven1(){
-        String result = FizzBuzzConverter.lister(1);
+        String result = FizzBuzzConverter.convert(1);
         Assert.assertEquals("1",result);
     }
 
     @Test
     public void FizzBuzzshouldReturn2whenGiven2(){
-        String result = FizzBuzzConverter.lister(2);
+        String result = FizzBuzzConverter.convert(2);
         Assert.assertEquals("2",result);
     }
 
 
     @Test
     public void FizzBuzzshouldReturnFizzwhenGiven3(){
-        String result = FizzBuzzConverter.lister(3);
+        String result = FizzBuzzConverter.convert(3);
         Assert.assertEquals("Fizz",result);
     }
 
     @Test
     public void FizzBuzzshouldReturnFizzwhenGiven6(){
-        String result = FizzBuzzConverter.lister(3);
+        String result = FizzBuzzConverter.convert(3);
         Assert.assertEquals("Fizz",result);
     }
 
     @Test
     public void FizzBuzzshouldReturnBuzzwhenGiven5(){
-        String result = FizzBuzzConverter.lister(5);
+        String result = FizzBuzzConverter.convert(5);
         Assert.assertEquals("Buzz",result);
     }
 
     @Test
     public void FizzBuzzshouldReturnBuzzwhenGiven10(){
-        String result = FizzBuzzConverter.lister(10);
+        String result = FizzBuzzConverter.convert(10);
         Assert.assertEquals("Buzz",result);
+    }
+
+    @Test
+    public void FizzBuzzshouldReturnFizzBuzzwhenGiven15(){
+        String result = FizzBuzzConverter.convert(15);
+        Assert.assertEquals("FizzBuzz",result);
     }
 
 }
