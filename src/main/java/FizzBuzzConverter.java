34ff26c1811084fc30f8ca195cd7a7571b787493
yassinenaciri <yassinen84@gmail.com>
2022-03-10 16:30:49 +0100

    second

--- src/main/java/FizzBuzzConverter.java
+++ src/main/java/FizzBuzzConverter.java
@@ -1,6 +1,6 @@
 public class FizzBuzzConverter {
-    public static String lister(int i) {
-
+    public static String convert(int i) {
+        if (i ==2) return "2";
         return "1";
     }
 }
--- src/test/java/FizzBuzzConverterTest.java
+++ src/test/java/FizzBuzzConverterTest.java
@@ -4,7 +4,13 @@ import org.junit.Test;
 public class FizzBuzzConverterTest {
     @Test
     public void FizzBuzzshouldReturn1whenGiven1(){
-        String result = FizzBuzzConverter.lister(1);
+        String result = FizzBuzzConverter.convert(1);
         Assert.assertEquals("1",result);
+    }
+
+    @Test
+    public void FizzBuzzshouldReturn2whenGiven2(){
+        String result = FizzBuzzConverter.convert(2);
+        Assert.assertEquals("2",result);
     }
 }
