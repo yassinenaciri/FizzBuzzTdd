dac09c126c890a43bb9723b45b4c87bf5a3f13a9
yassinenaciri <yassinen84@gmail.com>
2022-03-11 18:18:41 +0100

    list elements up to 15

--- src/test/java/FizzBuzzEngineTest.java
+++ src/test/java/FizzBuzzEngineTest.java
@@ -20,4 +20,10 @@ public class FizzBuzzEngineTest {
         String result = FizzBuzzEngine.generate(3);
         Assert.assertEquals("12Fizz",result);
     }
+
+    @Test
+    public void shouldReturn12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzzwhenGiven15(){
+        String result = FizzBuzzEngine.generate(15);
+        Assert.assertEquals("12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz",result);
+    }
 }
