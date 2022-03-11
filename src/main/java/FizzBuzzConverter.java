e3f79640ebe7c5c2472de1454e05e651d51beba7
yassinenaciri <yassinen84@gmail.com>
2022-03-11 17:50:00 +0100

    added case 30

--- src/main/java/FizzBuzzConverter.java
+++ src/main/java/FizzBuzzConverter.java
@@ -1,5 +1,5 @@
 public class FizzBuzzConverter {
-    public static String lister(int i) {
+    public static String convert(int i) {
         if (i%15 == 0) return "FizzBuzz";
         if (i %5 ==  0) return  "Buzz";
         if (i%3 == 0) return  "Fizz" ;
