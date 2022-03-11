d2e4a8b3579aac44ce2ac5bdc1766be415eb1504
yassinenaciri <yassinen84@gmail.com>
2022-03-11 17:54:20 +0100

    4

--- src/test/java/FizzBuzzConverterTest.java
+++ src/test/java/FizzBuzzConverterTest.java
@@ -4,32 +4,32 @@ import org.junit.Test;
 public class FizzBuzzConverterTest {
     @Test
     public void FizzBuzzshouldReturn1whenGiven1(){
-        String result = FizzBuzzConverter.lister(1);
+        String result = FizzBuzzConverter.convert(1);
         Assert.assertEquals("1",result);
     }
 
     @Test
     public void FizzBuzzshouldReturn2whenGiven2(){
-        String result = FizzBuzzConverter.lister(2);
+        String result = FizzBuzzConverter.convert(2);
         Assert.assertEquals("2",result);
     }
 
 
     @Test
     public void FizzBuzzshouldReturnFizzwhenGiven3(){
-        String result = FizzBuzzConverter.lister(3);
+        String result = FizzBuzzConverter.convert(3);
         Assert.assertEquals("Fizz",result);
     }
 
     @Test
     public void FizzBuzzshouldReturnFizzwhenGiven6(){
-        String result = FizzBuzzConverter.lister(3);
+        String result = FizzBuzzConverter.convert(3);
         Assert.assertEquals("Fizz",result);
     }
 
     @Test
     public void FizzBuzzshouldReturnBuzzwhenGiven5(){
-        String result = FizzBuzzConverter.lister(5);
+        String result = FizzBuzzConverter.convert(5);
         Assert.assertEquals("Buzz",result);
     }
 
